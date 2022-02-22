d4fef1edf71d76079dabbe0437ba7e6158d5b3b4
yanichik <brunshte@gmail.com>
2022-02-21 21:46:52 -0800

    signup form

---
+++ src/components/SignupForm.js
@@ -0,0 +1,17 @@
+const SignupForm = (props) => {
+	const submitHandler = () => {};
+	return (
+		<form onSubmit={submitHandler}>
+			<div>
+				<label htmlFor="username">Username</label>
+				<input type="text" id="username" />
+			</div>
+			<div>
+				<label htmlFor="password">Password</label>
+				<input type="text" id="password" />
+			</div>
+			<button type="submit">Signup</button>
+		</form>
+	);
+};
+export default SignupForm;
