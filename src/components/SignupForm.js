2bedd3ccc5ce802a0328e7596401cee53a3f11f1
yanichik <brunshte@gmail.com>
2022-02-21 22:22:22 -0800

    reset inputs to blank

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,18 +1,24 @@
+import { useRef } from "react";
 const SignupForm = (props) => {
+	const pwRef = useRef();
+	const userRef = useRef();
 	const submitHandler = (event) => {
 		event.preventDefault();
 		props.onSignup();
+		// reset inputs to blank
+		userRef.current.value = "";
+		pwRef.current.value = "";
 	};
 	return (
 		<div>
 			<form onSubmit={submitHandler}>
 				<div>
 					<label htmlFor="username">Username</label>
-					<input type="text" id="username" />
+					<input type="text" id="username" ref={userRef} />
 				</div>
 				<div>
 					<label htmlFor="password">Password</label>
-					<input type="password" id="password" />
+					<input type="password" id="password" ref={pwRef} />
 				</div>
 				<button type="submit">Signup</button>
 			</form>
